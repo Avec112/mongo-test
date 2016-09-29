25b4f17b92562d78078f2b9ab1b3644d60205646
Avec112 <avec112@gmail.com>
2016-09-30 01:40:33 +0200

    Added more to Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,33 +1,54 @@
 node {
-   def mvnHome
-   stage('Preparation') { // for display purposes
-      // Get some code from a GitHub repository
-      git 'https://github.com/Avec112/mongo-test.git'
-      // Get the Maven tool.
-      // ** NOTE: This 'M3' Maven tool must be configured
-      // **       in the global configuration.
-      mvnHome = tool 'M3'
-   }
-   stage('Build') {
-      // Run the maven build
-      sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
-      //sh "'${mvnHome}/bin/mvn' clean package"
-   }
-   //stage('Test') {
-     // sh "'${mvnHome}/bin/mvn' sonar:sonar"
-
-   //}
-   stage('Results') {
-       junit: '**/target/surefire-reports/TEST-*.xml'
-       archive: '**/target/*.jar'
-       //step([$class: 'ArtifactArchiver', artifacts: '**/target/*.jar', fingerprint: true])
-       //step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
-       /* parallel (
-           "text1":{
-           },
-           "text2":{
-           }
+    def mvnHome
+    stage('Preparation') { // for display purposes
+        // Get some code from a GitHub repository
+        git 'https://github.com/Avec112/mongo-test.git'
+        // Get the Maven tool.
+        // ** NOTE: This 'M3' Maven tool must be configured
+        // **       in the global configuration.
+        mvnHome = tool 'M3'
+    }
+    stage('Build') {
+        // Run the maven build
+        sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
+        //sh "'${mvnHome}/bin/mvn' clean package"
+    }
+    stage('QA') {
+        parallel(
+            "build-verification": {
+                junit: '**/target/surefire-reports/TEST-*.xml'
+                // jacoco?
+                archive: '**/target/*.jar'
+            },
+            "sonarqube": {
+                sh "'${mvnHome}/bin/mvn' sonar:sonar"
+                // TODO how to notify jenkins based on sonar error level...
+            }
         )
-        */
-   }
+    }
+    stage('Test') {
+        parallel(
+            "integration": {
+                // start docker mongodb
+            },
+            "performance": {
+                // run some performance testing
+            }
+        )
+    }
+    stage('Deploy') {
+        parallel(
+            "integration": {
+
+            },
+            "performance": {
+
+            }
+        )
+    }
+
+    //stage('Results') {
+    //    junit: '**/target/surefire-reports/TEST-*.xml'
+    //    archive: '**/target/*.jar'
+    //}
 }
